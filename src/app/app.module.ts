bf8d5b7124e829699bad0fddb3c3abd1ca7521f7
Wasif Zaman <teatocode@gmail.com>
2018-01-07 12:54:06 -0500

    stubbed out comps and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExhibitsComponent } from './exhibits/exhibits.component';
+import { SocialComponent } from './social/social.component';
+import { BackToTopComponent } from './back-to-top/back-to-top.component';
+import { ModalComponent } from './modal/modal.component';
+import { ModalService } from './modal.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ExhibitsComponent,
+    SocialComponent,
+    BackToTopComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
